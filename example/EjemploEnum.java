aba425f2d0e06a3e9358707a6cc932550f91c5aa
wilder5912 <wilder.5912@gmail.com>
2018-05-06 00:03:19 -0400

    update file

---
+++ example/EjemploEnum.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class llll - write a description of the enum class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum EjemploEnum
+{
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
--- example/exxtra/redondo.java
+++
@@ -1,9 +0,0 @@
-package exxtra;
-
-import Rectangulo;
-
-public class redondo implements Figura{
-
-public float area() { return (PI*diametro*diametro/4f); }
-
-}
